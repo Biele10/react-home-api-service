d406cc4977db06327f0a2475480690275ae596ea
Biele <158857885+Biele10@users.noreply.github.com>
2026-05-08 21:02:01 +0100

    forcing my tiny stupid brain to understnad how to properly create a hashtable in C++.

--- backend/code/arduino/embedded-code/src/utilities/hashStruct.cpp
+++ backend/code/arduino/embedded-code/src/utilities/hashStruct.cpp
@@ -4,33 +4,4 @@
 
 class hashTable
 {
-    private:
-
-        /**
-         * Structure for a hash table entry.
-         */
-        struct entry
-        {
-            String key;
-            String value;
-            entry* nextEntry;       // allows for a linked list
-        };
-
-        static const uint8_t size = Config::BASE_HASH_TABLE_SIZE;
-        entry table[size];
-
-        uint8_t hash(hashTable& ht, const String& key);
-}
-
-uint8_t hash(hashTable& ht, const String& key)
-{
-    uint8_t hash = 0;
-    for (uint8_t i=0; i < key.length(); i++)
-    {
-        hash = (hash * 31 + key[i] % ht.size);
-    }
-
-    return hash;
-}
-
-void insert()
+};
