c37b6b1112ad52b3775ceb861f34967a04b0d282
Biele <158857885+Biele10@users.noreply.github.com>
2026-05-24 22:29:02 +0100

    Added class for the shelly switch.

---
+++ backend/code/hardware/ShellySwitch.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\hardware;
+
+/**
+ * Class represents a shelly dimmer switch which
+ * is used to control lights.
+ */
+Class ShellySwitch
+{
+    public function __construct(int $pinNumber)
+    {}
+}
